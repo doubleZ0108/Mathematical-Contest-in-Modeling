865afe471761eb8b357c79322626e53cacbde468
double Z <doubleZ0108@163.com>
2020-09-19 20:31:06 +0800

    :pencil:toc adapt

--- 2019国赛 | CUMCM/代码/HTML + JavaScript/static/js/common.js
+++ 2019国赛 | CUMCM/代码/HTML + JavaScript/static/js/common.js
@@ -1,45 +1,45 @@
 window.onload = function () {
-    let left = 120 + 52/60;
-    let right = 121 + 19/60;
-    let top = 31 + 53/60;
-    let bottom = 30 + 40/64;
+    let left = 120 + 52 / 60;
+    let right = 121 + 19 / 60;
+    let top = 31 + 53 / 60;
+    let bottom = 30 + 40 / 64;
 
-    let gap_lr = (right-left)/6;
-    let gap_tb = (top-bottom)/6;
+    let gap_lr = (right - left) / 6;
+    let gap_tb = (top - bottom) / 6;
 
     let hor = new Array();
     let ver = new Array();
 
-    for(let i=0;i<7;++i){
-        hor[i] = String((left + gap_lr*i).toFixed(4));
+    for (let i = 0; i < 7; ++i) {
+        hor[i] = String((left + gap_lr * i).toFixed(4));
     }
-    for(let j=0;j<7;++j){
-        ver[j] = String((top - gap_tb*j).toFixed(4));
+    for (let j = 0; j < 7; ++j) {
+        ver[j] = String((top - gap_tb * j).toFixed(4));
     }
     console.log(hor);
     console.log(ver);
 
 
     let T = document.getElementById('mytabel');
-    for(let i=0;i<6;++i){
+    for (let i = 0; i < 6; ++i) {
         let tr = document.createElement('tr');
-        for(let j=0;j<6;++j){
+        for (let j = 0; j < 6; ++j) {
             let td = document.createElement('td');
 
 
             let TT = document.createElement('tabel');
 
-            for(let k=0;k<3;++k){
+            for (let k = 0; k < 3; ++k) {
                 let trtr = document.createElement('tr');
-                if(k==0){
+                if (k == 0) {
                     trtr.innerHTML = '<td></td>\n' +
                         '<td class="top">' + String(ver[i]) + '</td>\n' +
                         '<td></td>';
-                }else if(k==1){
-                    trtr.innerHTML = '<td class="left">'+String(hor[j])+'</td>\n' +
+                } else if (k == 1) {
+                    trtr.innerHTML = '<td class="left">' + String(hor[j]) + '</td>\n' +
                         '<td></td>\n' +
-                        '<td class="right">'+String(hor[j+1])+'</td>'
-                }else{
+                        '<td class="right">' + String(hor[j + 1]) + '</td>'
+                } else {
                     trtr.innerHTML = '<td></td>\n' +
                         '<td class="bottom">' + String(ver[i + 1]) + '</td>\n' +
                         '<td></td>';
--- 2019国赛 | CUMCM/代码/python/main.py
+++ 2019国赛 | CUMCM/代码/python/main.py
@@ -1,37 +1,39 @@
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.use('TkAgg')
 import random
-from threading import Thread,Timer
-from time import sleep,time
+from threading import Thread, Timer
+from time import sleep, time
 import numpy as np
 import sys
 
-
 customers = []
 waitTime = 0.5
-STATE = 'DAY'       # 'NIGHT
+STATE = 'DAY'  # 'NIGHT
+
 
 def getCustomerNum():
     tmp = random.randint(1, 100)
-    if tmp>=1 and tmp<=60:
+    if tmp >= 1 and tmp <= 60:
         return 1
-    elif tmp>=61 and tmp<=92:
+    elif tmp >= 61 and tmp <= 92:
         return 2
-    elif tmp>=93 and tmp<=98:
+    elif tmp >= 93 and tmp <= 98:
         return 3
-    elif tmp==99 or tmp==100:
+    elif tmp == 99 or tmp == 100:
         return 4
 
+
 def growCustomer():
-    time_per_planeNum = 60/33
+    time_per_planeNum = 60 / 33
     stepTime = random.uniform(0, time_per_planeNum)
-    sleep(stepTime/10)
+    sleep(stepTime / 10)
 
     # customer_mu = 36.833 if STATE=='DAY' else 110.488
     # customer_sigma = 7.488 if STATE=='DAY' else 22.464
-    customer_mu = 36.688/6
-    customer_sigma = 7.488/6
+    customer_mu = 36.688 / 6
+    customer_sigma = 7.488 / 6
 
     customerNum = int(np.random.normal(customer_mu, customer_sigma, 1)[0])
 
@@ -42,17 +44,17 @@ def growCustomer():
     timer = Timer(0.01, growCustomer)
     timer.start()
 
-def getWaitTime(pos):
 
+def getWaitTime(pos):
     global customers
     customers.clear()
     print(pos)
 
     total_waitTime = 0
 
-    while pos>0:
+    while pos > 0:
         # print(pos)
-        sleep(waitTime/100)
+        sleep(waitTime / 100)
         total_waitTime += waitTime
 
         if customers:
@@ -72,13 +74,13 @@ def getWaitTime(pos):
             customers = customers[customerNum:]
 
             pos -= 1
-            total_waitTime += end-start
+            total_waitTime += end - start
 
     return total_waitTime
 
 
 def main():
-    N = 100      # 蓄车池最大容量
+    N = 100  # 蓄车池最大容量
     pos_x = [i for i in range(1, N)]
     waitTime_y = list(map(getWaitTime, pos_x))
 
@@ -95,6 +97,5 @@ def main():
 '''启动全局定时器'''
 timer = Timer(0, growCustomer).start()
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
